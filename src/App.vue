02cc6f28dac65e477503e00dce660fe96ad954d1
Eugene Demotte <eugenedelain@gmail.com>
2021-04-07 15:39:50 +0530

    first commit
    orgchart.js implemented using vue.js

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div>
+      <organization-chart :datasource="ds"></organization-chart>
+    </div>
+  </div>
+</template>
+
+<script>
+  import OrganizationChart from 'vue-organization-chart'
+  import 'vue-organization-chart/dist/orgchart.css'
+  import orgData from "./datasource/organization-data.json"
+
+export default {
+  name: 'App',
+  components: {
+    OrganizationChart
+  },
+      data () {
+      return {
+        ds: orgData
+      }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
